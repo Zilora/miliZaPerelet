87a8603feb16b185b8368140f858149592c11c2c
Zilora <minimbaeva.zilura@yandex.ru>
2022-05-20 15:13:27 +0500

    change

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,15 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("Акция:бесплатные мили за перелеты");
-        int ticket = 6000; // рублей за билет
-        int price = 20; // рублей за 1 бонус
-        // System.out.println(ticketPrice / priceFor1mile);
-        int bonus = 300;
-        System.out.println("За каждые 6000 рублей при покупке билета, будет начисляться 300 бонусов");
+        int ticket = 30000; // рублей за билет
+        int price = 20;
+        if (ticket<6000) {
+            System.out.println("Недостаточно суммы для начисления бонуса");
+        }
+        else if (ticket>=6000) {
+            System.out.println(ticket/price);
+            System.out.println("бонусов начислится");
+            
+        }
     }
 }
